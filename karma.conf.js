ec183bf6e1aa7b33fec940e38168aaed82e8dcb0
Jennie 'sevvie' Rose <sevvie.rose@gmail.com>
2016-06-07 19:14:06 -0700

    Added karma.conf.js and the jasmine/angular setup file.

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(c) {
+  c.set({
+    basePath: '',
+    frameworks: ['browserify', 'source-map-support', 'jasmine'],
+    files: [
+      'test/init.js',
+      'test/**/*.spec.js'
+    ],
+    exclude: [],
+    preprocessors: {
+      'test/init.js': ['browserify'],
+      'test/**/*.spec.js': ['browserify']
+    },
+    browserify: {
+      debug: true,
+      transform: ['babelify']
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: c.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    concurrency: Infinity
+  });
+}
